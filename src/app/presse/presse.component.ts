9d118895d4a092827012eced4ff6d79c206e2447
Aurelien Delay <aurelien.delay@gmail.com>
2020-01-13 16:15:20 +0100

    ajout article inauguration local de campagne

--- src/app/presse/presse.component.ts
+++ src/app/presse/presse.component.ts
@@ -10,6 +10,7 @@ export class PresseComponent implements OnInit {
   constructor() { }
 
   articles: any[] = [
+    {journal: 'Nice Matin', date: '12 Janvier 2020', image: 'nicematin_12janvier2020.jpg', alt: 'A Antibes, l\'Alternative ouvre son local de campagne'},
     {journal: 'Nice Matin', date: '08 Janvier 2020', image: 'nicematin_08janvier2020.jpg', alt: 'Cécile Dumas appelle les élus à soutenir financièrement les grévistes'},
     {journal: 'Nice Matin', date: '17 Décembre 2019', image: 'nicematin_20dec_programme.jpg', alt: 'Les volontés d\'Arnaud Delcasse pour Antibes'},
     {journal: 'Nice Matin', date: '28 Novembre 2019', image: 'nicematin_28nov_tetedeliste.jpg', alt: 'Arnaud Delcasse veut être le visage de la gauche unie'},
